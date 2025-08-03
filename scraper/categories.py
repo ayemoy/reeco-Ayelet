12af467c33bbf2e47695ea0196aabe7d08d5add7
ayemoy <ayelet431@gmail.com>
2025-08-03 14:47:09 +0300

    fixes

--- scraper/categories.py
+++ scraper/categories.py
@@ -39,31 +39,30 @@ async def click_category_by_name(page: Page, category_name: str):
 
 # async def visit_each_category(page: Page) -> None:
 #     categories_url = page.url  
-#     print("🔍 🔍🔍categories_url:", categories_url)
+#     print("categories_url:", categories_url)
 
 #     categories = await get_all_categories(page)
 
-#     # ✅ הרץ רק קטגוריה אחת לצורך בדיקה:
+#     # run one category for debug
 #     for cat in categories:
-#         if cat["name"] != "Chemicals":  # החלף לקטגוריה שאת רוצה לבדוק
+#         if cat["name"] != "Chemicals":  
 #             continue
 
 #         await click_category_by_name(page, cat["name"])
-#         print(f"🔍 Inside category: {cat['name']}")
+#         print(f" Inside category: {cat['name']}")
 
 #         await scrape_all_products_in_category(page, cat["name"])
 
-#         print(f"🔁 Going back to categories page...")
+#         print(f" Going back to categories page...")
 #         await page.goto(categories_url)
 
 #         try:
 #             await page.wait_for_selector("span.category-grid-title", timeout=15000)
 #         except Exception as e:
-#             print(f"⚠️ Failed waiting for categories page: {e}")
+#             print(f"Failed waiting for categories page: {e}")
 #             return
 
-#         break  # ✅ סיים אחרי הקטגוריה הראשונה לבדיקה
-
+#         break  # 
 
 
 
@@ -78,26 +77,25 @@ async def click_category_by_name(page: Page, category_name: str):
 
 async def visit_each_category(page: Page) -> None:
     categories_url = page.url  
-    print("🔍 🔍🔍categories_url:",categories_url)
+    print("categories_url:",categories_url)
     categories = await get_all_categories(page)
 
     for index, cat in enumerate(categories):
         await click_category_by_name(page, cat["name"])
-        print(f"🔍 Inside category: {cat['name']}")
+        print(f" Inside category: {cat['name']}")
 
         await scrape_all_products_in_category(page, cat["name"])
 
-        print(f"🔁 Going back to categories page...")
+        print(f"Going back to categories page...")
         await page.goto(categories_url)
         
-        # חשוב! חכי לאלמנט שמעיד שהעמוד נטען לגמרי
+
         try:
             await page.wait_for_selector("span.category-grid-title", timeout=15000)
         except Exception as e:
-            print(f"⚠️ Failed waiting for categories page: {e}")
+            print(f" Failed waiting for categories page: {e}")
             return
 
-        # בגלל שהעמוד נטען מחדש – צריך לשלוף את האלמנטים מחדש
         categories = await get_all_categories(page)
 
 
@@ -106,17 +104,15 @@ async def visit_each_category(page: Page) -> None:
 
 
 # async def visit_each_category(page: Page) -> None:
-#     # שמירת כתובת עמוד הקטגוריות הראשי
 #     categories_url = page.url  
 
 #     categories = await get_all_categories(page)
 
 #     for cat in categories:
 #         await click_category_by_name(page, cat["name"])
-#         print(f"🔍 Inside category: {cat['name']}")
+#         print(f"Inside category: {cat['name']}")
 
 #         await scrape_all_products_in_category(page, cat["name"])
 
-#         # חזרה לעמוד הראשי של הקטגוריות
 #         await page.goto(categories_url)
 #         await page.wait_for_selector("span.category-grid-title", timeout=10000)
